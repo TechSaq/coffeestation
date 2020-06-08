8776618b52e98321f7c1e0e91e51d1a01eb26c12
Mohd Saquib <ans2sqb@gmail.com>
2020-06-08 23:33:19 +0530

    initial commit

---
+++ coffeestation/settings/development.py
@@ -0,0 +1,16 @@
+from .base import *
+
+DEBUG = True
+
+ALLOWED_HOSTS = []
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'saquibdb',
+        'USER': 'saquib',
+        'PASSWORD': '.saqu.',
+        'HOST': 'localhost',
+        'PORT': '5432',
+    }
+}
