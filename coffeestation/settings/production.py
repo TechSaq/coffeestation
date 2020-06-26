5cade699d0787696af7c66c024e2e35a429bd9f5
Mohd Saquib <ans2sqb@gmail.com>
2020-06-26 09:23:19 +0530

    static files hosting configuration for aws s3

--- coffeestation/settings/production.py
+++ coffeestation/settings/production.py
@@ -2,3 +2,18 @@ from .base import *
 
 DEBUG = False
 
+INSTALLED_APPS += [
+    'storages',
+]
+
+AWS_ACCESS_KEY_ID = config('AWS_ACCESS_KEY_ID')
+AWS_SECRET_ACCESS_KEY = config('AWS_SECRET_ACCESS_KEY')
+AWS_STORAGE_BUCKET_NAME = config('AWS_STORAGE_BUCKET_NAME')
+AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
+AWS_S3_OBJECT_PARAMETERS = {'CacheControl': 'max-age=86400',
+                            }
+AWS_LOCATION = 'static'
+STATIC_URL = 'https://%s/%s/' % (AWS_S3_CUSTOM_DOMAIN, AWS_LOCATION)
+STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+
+DEFAULT_FILE_STORAGE = 'coffeestation.storage_backends.MediaStorage'
