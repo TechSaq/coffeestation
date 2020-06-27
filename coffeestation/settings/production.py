50c28662b49bbf82f95e435968bc52caf642eaf4
Mohd Saquib <ans2sqb@gmail.com>
2020-06-27 22:53:17 +0530

    added / in urls:shop

--- coffeestation/settings/production.py
+++ coffeestation/settings/production.py
@@ -36,3 +36,36 @@ CSRF_COOKIE_SECURE = True
 SECURE_REFERRER_POLIC = 'same-origin'
 
 django_heroku.settings(locals())
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'formatters': {
+        'verbose': {
+            'format': "[%(asctime)s] %(levelname)s [%(name)s:%(lineno)s] %(message)s",
+            'datefmt': "%d/%b/%Y %H:%M:%S"
+        },
+        'simple': {
+            'format': '%(levelname)s %(message)s'
+        },
+    },
+    'handlers': {
+        'file': {
+            'level': 'DEBUG',
+            'class': 'logging.FileHandler',
+            'filename': 'mysite.log',
+            'formatter': 'verbose'
+        },
+    },
+    'loggers': {
+        'django': {
+            'handlers': ['file'],
+            'propagate': True,
+            'level': 'DEBUG',
+        },
+        'MYAPP': {
+            'handlers': ['file'],
+            'level': 'DEBUG',
+        },
+    }
+}
