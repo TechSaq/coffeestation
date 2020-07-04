ca61321a307dc3698d9ea91df44dfbb8f6d7b5ca
Mohd Saquib <ans2sqb@gmail.com>
2020-07-04 19:10:58 +0530

    manifest strict false

--- coffeestation/settings/production.py
+++ coffeestation/settings/production.py
@@ -20,7 +20,7 @@ AWS_LOCATION = 'static'
 STATIC_URL = 'https://%s/%s/' % (AWS_S3_CUSTOM_DOMAIN, AWS_LOCATION)
 # STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 # STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
-STATICFILES_STORAGE = 'offeestation.storage_backends.WhiteNoiseStaticFilesStorage'
+STATICFILES_STORAGE = 'coffeestation.storage_backends.WhiteNoiseStaticFilesStorage'
 
 DEFAULT_FILE_STORAGE = 'coffeestation.storage_backends.MediaStorage'
 
