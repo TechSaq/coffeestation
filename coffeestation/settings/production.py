68ca06bc7862861f2583ee5290c6ddcf6709e53a
Mohd Saquib <ans2sqb@gmail.com>
2020-07-04 18:54:28 +0530

    added custom static storage

--- coffeestation/settings/production.py
+++ coffeestation/settings/production.py
@@ -18,9 +18,10 @@ AWS_S3_OBJECT_PARAMETERS = {'CacheControl': 'max-age=86400',
                             }
 AWS_LOCATION = 'static'
 STATIC_URL = 'https://%s/%s/' % (AWS_S3_CUSTOM_DOMAIN, AWS_LOCATION)
-STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+# STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 
 DEFAULT_FILE_STORAGE = 'coffeestation.storage_backends.MediaStorage'
+STATICFILES_STORAGE = 'coffeestation.storage_backends.StaticStorage'
 
 SECURE_SSL_REDIRECT = True
 SECURE_SSL_EXEMPT = []
--- coffeestation/storage_backends.py
+++ coffeestation/storage_backends.py
@@ -4,3 +4,8 @@ from storages.backends.s3boto3 import S3Boto3Storage
 class MediaStorage(S3Boto3Storage):
     location = 'media'
     file_overwrite = False
+
+
+class StaticStorage(S3Boto3Storage):
+    bucket_name = 'coffeestationbucket'
+    location = 'static'
